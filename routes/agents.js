b05399be465ef8a81c7a5c34bcb7f747806bdc0c
rabchev <boyan@rabchev.com>
2013-01-16 17:14:34 +0200

    more pages

--- routes/agents.js
+++ routes/agents.js
@@ -1,10 +1,20 @@
 /*jslint plusplus: true, devel: true, nomen: true, vars: true, node: true, indent: 4, maxerr: 50 */
 /*global require, exports, module */
 
+var path    = require("path"),
+    agents  = require(path.join(__dirname, "..", "data", "agents.json")),
+    idx     = 0,
+    data    = {
+        title: "Agents",
+        rows: []
+    };
+
+while (agents.length) {
+    data.rows[idx++] = agents.splice(0, 3);
+}
+
 exports.get = function (req, res) {
     "use strict";
     
-    res.render('agents', {
-        title: 'Agents'
-    });
+    res.render("agents", data);
 };
--- routes/forSale.js
+++ routes/forSale.js
@@ -1,10 +1,16 @@
 /*jslint plusplus: true, devel: true, nomen: true, vars: true, node: true, indent: 4, maxerr: 50 */
 /*global require, exports, module */
 
+var path    = require("path"),
+    props  = require(path.join(__dirname, "..", "data", "for-sale.json")),
+    idx     = 0,
+    data    = {
+        title: "For Sale",
+        rows: props
+    };
+
 exports.get = function (req, res) {
     "use strict";
     
-    res.render('forSale', {
-        title: 'For Sale'
-    });
+    res.render('forSale', data);
 };
